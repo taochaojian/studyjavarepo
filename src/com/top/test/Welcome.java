94888095378cc05b00661f269894c53e74136102
taochaojian <taochaojian@yeah.net>
2021-11-29 00:00:18 +0800

    添加了Welcome测试代码

---
+++ src/com/top/test/Welcome.java
@@ -0,0 +1,13 @@
+package com.top.test;
+
+public class Welcome {
+
+    public static  void  main(String[] args)
+    {
+        String greeting="Welcome to Core  JAVA";
+        System.out.println(greeting);
+        for(int i=0;i<greeting.length();i++)
+            System.out.print("=");
+        System.out.println();
+    }
+}
